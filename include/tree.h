4d26c35a23d737c81954650c885a47a20b2ce02f
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-05-31 14:00:55 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -12,13 +12,37 @@ struct Node {
 };
 
 class Tree {
+ public:
+    explicit Tree(const std::vector<char>& init):root(nullptr) {
+        createPerm(init);
+    }
+    std::vector<std::vector<char>> getpermutations() const {
+        return permutations;
+    }
+
  private:
     Node* root;
     std::vector<std::vector<char>> permutations;
+
     void createPerm(const std::vector<char>& posl) {
         addLil(root, posl);
         readTree(root, {});
     }
+
+    void addLil(Node* ptr, const std::vector<char>& posl) {
+        if (ptr == nullptr) {
+            root = ptr = new Node;
+        }
+        for (char num : posl) {
+            Node* tm = new Node(num);
+            ptr->lil.push_back(tm);
+            std::vector<char> updatedposl(posl);
+            updatedposl.erase \
+                (std::find(updatedposl.begin(), updatedposl.end(), num));
+            addLil(tm, updatedposl);
+        }
+    }
+
     void readTree(Node* ptr, std::vector<char> posl) {
         if (ptr != nullptr && ptr->num != '\0') posl.push_back(ptr->num);
         if (ptr->lil.empty()) permutations.push_back(posl);
@@ -26,27 +50,5 @@ class Tree {
             readTree(pot, posl);
         }
     }
-    void addLil(Node* ptr, const std::vector<char>& posl) {
-        if (ptr == nullptr) {
-            root = ptr;
-            root = new Node;
-        }
-        for (char num : posl) {
-            Node* tm = new Node(num);
-            ptr->lil.push_back(tm);
-            std::vector<char> updatedPosl(posl);
-            updatedPosl.erase \
-                (std::find(updatedPosl.begin(), updatedPosl.end(), num));
-            addLil(tm, updatedPosl);
-        }
-    }
-
- public:
-     explicit Tree(const std::vector<char>& init):root(nullptr) {
-         createPerm(init);
-     }
-     std::vector<std::vector<char>> getpermutations() const {
-         return permutations;
-     }
 };
 #endif  // INCLUDE_TREE_H_
